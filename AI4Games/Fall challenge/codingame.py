a341a03be3cbb3fc7eed710ed022aed7281beeb5
Jakub Grodzicki <kubagrodzicki@o2.pl>
2020-11-15 17:52:57 +0100

    AI4G

--- AI4Games/Fall challenge/codingame.py
+++ AI4Games/Fall challenge/codingame.py
@@ -1,13 +1,15 @@
 '''
 Implemented logic:
-    - if possible brew the most valuable potion (only the top one)
+    - for fixed amount of rounds learn new spells
+    
+    - if possible brew the most valuable potion (or any - depending on the parametere)
     - else:
-        - find the cast with the most valuable ingredient you don't have and cast a spell to get it
-        - else rest
+        - run a BFS to find which spell to cast for the fastest way of getting to brewing a potion (most val. or any)
 '''
+from collections import deque
 import sys
 import math
-from typing import NamedTuple, Dict, Optional, Union, List, Tuple
+from typing import NamedTuple, Dict, Optional, Union, List, Tuple, Deque
 import numpy as np  # type: ignore
 from enum import Enum
 from itertools import combinations
@@ -33,7 +35,7 @@ Recipe = NamedTuple('Recipe', (
     ('price', int),
 ))
 
-Spell = NamedTuple('Cast', (
+Spell = NamedTuple('Spell', (
     ('ingredients', np.ndarray),
     ('castable', bool),
     ('repeatable', bool),
@@ -51,12 +53,15 @@ class ActionType(Enum):
 class Action(NamedTuple):
     type: ActionType
     id: Optional[int] = None
+    times_used: Optional[int] = None
     comment: Optional[str] = None
 
     def __repr__(self):
         s = self.type.name
         if self.id is not None:
             s += f' {self.id}'
+        if self.times_used is not None:
+            s += f' {self.times_used}'
         if self.comment is not None:
             s += f' {self.comment}'
         return s
@@ -64,19 +69,20 @@ class Action(NamedTuple):
 
 class Agent:
 
-    def __init__(self, max_known_spells: int) -> None:
+    def __init__(self, max_known_spells: int, go_for_most_valuable: bool) -> None:
         self.my_state = State(inventory=np.array([0, 0, 0, 0]), score=0)
         self.opp_state = State(inventory=np.array([0, 0, 0, 0]), score=0)
 
-        self.brew_receipes: Dict[int, Recipe] = {}
+        self.brew_recipes: Dict[int, Recipe] = {}
         self.spells: Dict[int, Spell] = {}
         self.to_learn: Dict[int, Spell] = {}
         
         self.max_known_spells = max_known_spells
+        self.go_for_most_valuable = go_for_most_valuable
 
 
     def get_input(self) -> None:
-        self.brew_receipes = {}
+        self.brew_recipes = {}
         self.spells = {}
         self.to_learn = {}
 
@@ -91,7 +97,7 @@ class Agent:
             castable, repeatable = map(lambda s: s != '0', input_values[9:11])
 
             if action_type == 'BREW':
-                self.brew_receipes[action_id] = Recipe(ingredients=deltas, price=price)
+                self.brew_recipes[action_id] = Recipe(ingredients=deltas, price=price)
             if action_type == 'CAST':
                 self.spells[action_id] = Spell(ingredients=deltas, castable=castable, repeatable=repeatable)
             if action_type == 'LEARN':
@@ -105,32 +111,21 @@ class Agent:
 
 
     def find_action(self) -> Action:
-        top_recipe_id, top_recipe = sorted(self.brew_receipes.items(), key=lambda kv: kv[1].price)[-1]
-        # for recipe_id, recipe in sorted_recipes:
-        if self.can_brew_or_cast(inventory=self.my_state.inventory, recipe_or_spell=top_recipe):
-            return Action(type=ActionType.BREW, id=top_recipe_id)
+        if self.go_for_most_valuable:
+            top_recipe_id, top_recipe = sorted(self.brew_recipes.items(), key=lambda kv: kv[1].price)[-1]
+            
+            if self.can_brew_or_cast(inventory=self.my_state.inventory, recipe_or_spell=top_recipe):
+                return Action(type=ActionType.BREW, id=top_recipe_id)
+        else:
+            for id, brew_recipe in sorted(self.brew_recipes.items(), key=lambda kv: kv[1].price, reverse=True):
+                if self.can_brew_or_cast(inventory=self.my_state.inventory, recipe_or_spell=brew_recipe):
+                    return Action(type=ActionType.BREW, id=id)
         
         # Learn a cast if less than max_known_spells are known.
         if len(self.spells.items()) < self.max_known_spells:
             return Action(type=ActionType.LEARN, id=list(self.to_learn.items())[0][0])
-
-        # Can't brew any recipes, get ingredients to the most valuable one.
-        most_valuable_missing_ingredient = np.nonzero((self.my_state.inventory + top_recipe.ingredients) < 0)[0][-1]
-
-        for ingredient in range(most_valuable_missing_ingredient, -1, -1):
-            possible_cast_item = list(
-                filter(lambda cast: cast[1].ingredients[ingredient] > 0, self.spells.items())
-            )
-
-            if len(possible_cast_item) > 0:
-                if self.can_brew_or_cast(self.my_state.inventory, possible_cast_item[0][1]):
-                    return Action(
-                        type=ActionType.CAST,
-                        id=possible_cast_item[0][0],
-                        comment=f'missing {most_valuable_missing_ingredient} | will get {ingredient}',
-                    )
-
-        return Action(type=ActionType.REST)
+        
+        return self.BFS()
 
 
     def can_brew_or_cast(self, inventory: np.ndarray, recipe_or_spell: Union[Recipe, Spell]) -> bool:
@@ -139,7 +134,97 @@ class Agent:
         
         new_inventory = inventory + recipe_or_spell.ingredients
         return np.all(new_inventory >= 0) and np.sum(new_inventory) <= 10
+    
+    
+    def BFS(self) -> Action:
+        '''
+        :param find_to_first: try to brew the first possible potion, if false will go for the most valuable one
+        :return: first move to get to obtain the inventory to be able to brew a potion
+        '''
+        q: Deque = deque()
+        q.append((self.my_state.inventory.copy(), self.spells.copy(), None))
+        
+        brew_recipes_ingredients = np.array([recipe.ingredients for recipe in self.brew_recipes.values()])
+        brew_recipes_prices = [recipe.price for recipe in self.brew_recipes.values()]
+        best_brew_recipe = list(self.brew_recipes.values())[np.argmax(brew_recipes_prices)]
 
+        cnt = 0
+        while len(q) > 0:
+            cnt += 1
+            log(cnt)
+            
+            inventory, spells, first_action = q.popleft()
+            # REST
+            q.append((
+                inventory,
+                self._spells_after_rest(spells=spells),
+                first_action if first_action is not None else Action(type=ActionType.REST),
+            ))
+            
+            # CAST a spell
+            for id, spell in spells.items():
+                if spell.castable and self.can_brew_or_cast(inventory=inventory, recipe_or_spell=spell):
+                    new_inventory = inventory.copy()
+                    new_inventory += spell.ingredients
+
+                    can_brew = np.all(new_inventory + brew_recipes_ingredients >= 0, axis=1)
+
+                    if self.go_for_most_valuable and self.can_brew_or_cast(new_inventory, best_brew_recipe):
+                        return first_action if first_action is not None \
+                            else Action(ActionType.CAST, id=id)
+                    elif not self.go_for_most_valuable and any(can_brew):
+                        return first_action if first_action is not None \
+                            else Action(ActionType.CAST, id=id)
+                    
+                    q.append((
+                        new_inventory,
+                        self._spells_after_cast(spells=spells, cast_id=id),
+                        first_action if first_action is not None else Action(type=ActionType.CAST, id=id, times_used=1),
+                    ))
+                    
+                    if spell.repeatable:
+                        times_used = 2
+                        while self.can_brew_or_cast(inventory=new_inventory, recipe_or_spell=spell):
+                            new_inventory += spell.ingredients
+                            can_brew = np.all(new_inventory >= brew_recipes_ingredients, axis=1)
+
+                            if self.go_for_most_valuable and self.can_brew_or_cast(new_inventory, best_brew_recipe):
+                                return first_action if first_action is not None \
+                                    else Action(ActionType.CAST, id=id, times_used=times_used)
+                            elif not self.go_for_most_valuable and any(can_brew):
+                                return first_action if first_action is not None \
+                                    else Action(ActionType.CAST, id=id, times_used=times_used)
+                                
+                            q.append((
+                                new_inventory,
+                                self._spells_after_cast(spells=spells, cast_id=id),
+                                first_action if first_action is not None else Action(
+                                    type=ActionType.CAST,
+                                    id=id,
+                                    times_used=times_used,
+                                ),
+                            ))
+                            times_used += 1
+        return Action(type=ActionType.REST, comment='No i chuj')
+        
+        
+    def _spells_after_rest(self, spells: Dict[int, Spell]) -> Dict[int, Spell]:
+        new_spells = {}
+        for id, spell in spells.items():
+            new_spells[id] = Spell(ingredients=spell.ingredients, castable=True, repeatable=spell.repeatable)
+        return new_spells
+    
+    
+    def _spells_after_cast(self, spells: Dict[int, Spell], cast_id: int) -> Dict[int, Spell]:
+        new_spells = {}
+        for id, spell in spells.items():
+            new_spells[id] = Spell(
+                ingredients=spell.ingredients,
+                castable=spell.castable if id != cast_id else False,
+                repeatable=spell.repeatable,
+            )
+        return new_spells
+    
 
     def run(self) -> None:
         self.get_input()
@@ -147,7 +232,7 @@ class Agent:
         print(action)
 
 
-agent = Agent(max_known_spells=10)
+agent = Agent(max_known_spells=10, go_for_most_valuable=False)
 
 while True:
     agent.run()
