346a243ac8198d2ca0a97d8af7582a2a3f6d19d7
Jakub Grodzicki <kubagrodzicki@o2.pl>
2020-12-12 18:39:22 +0100

    AI4G

--- AI4Games/Assignment 4/jps.py
+++ AI4Games/Assignment 4/jps.py
@@ -68,7 +68,7 @@ class JPS:
             self._distances[(column, row)] = Distance(N=n, NE=ne, E=e, SE=se, S=s, SW=sw, W=w, NW=nw)
         
         self._heap = Heap()
-        heuristic = measure_distance(self._position, self._goal_position)
+        heuristic = heuresis_distance(self._position, self._goal_position)
         for dir in ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']:
             if self._distances[self._position].__dict__[dir] != 0:
                 self._heap.push(heuristic, (self._position[0], self._position[1], dir), 0)
@@ -78,6 +78,17 @@ class JPS:
         # Flag set when found path to the goal position. If true, run function just prints the next states.
         self._found_path = False
         self._no_path = False
+        
+        self._dirs_to_check = {
+            'S': ['W', 'SW', 'S', 'SE', 'E'],
+            'SE': ['S', 'SE', 'E'],
+            'E': ['S', 'SE', 'E', 'NE', 'N'],
+            'NE': ['E', 'NE', 'N'],
+            'N': ['E', 'NE', 'N', 'NW', 'W'],
+            'NW': ['N', 'NW', 'W'],
+            'W': ['N', 'NW', 'W', 'SW', 'S'],
+            'SW': ['W', 'SW', 'S']
+        }
 
     
     def _run(self, time_limit: float) -> None:
@@ -94,55 +105,68 @@ class JPS:
                 self._heap.push(-np.inf, (x, y, dir), cost_from_start)
                 log('FOUND ONE!!!')
                 return
-            if position == (3, 2):
-                log(heuristic_value)
-                log(cost_from_start)
-            distance = self._distances[position].__dict__[dir]
-            xd = yd = 0
-            if 'N' in dir:
-                yd = -1
-            if 'S' in dir:
-                yd = 1
-            if 'E' in dir:
-                xd = 1
-            if 'W' in dir:
-                xd = -1
             
-            distance = abs(distance)  # Ignore the fact it can be to the wall.
-            
-            for i in range(1, distance+1):
-                tmp_position = (position[0] + i*xd, position[1] + i*yd)
-                if tmp_position == self._goal_position:
-                    tmp_cost_from_start = cost_from_start + measure_distance(position, tmp_position)
-                    self._heap.push(tmp_cost_from_start + measure_distance(position, self._goal_position), (self._goal_position[0], self._goal_position[1], '0'), tmp_cost_from_start)
-                    self._traversing[(self._goal_position, tmp_cost_from_start)] = (position, cost_from_start)
-                    
-                # if tmp_position not in self._visited:
-                #     tmp_cost_from_start = cost_from_start + measure_distance(position, tmp_position)
-                #
-                #     self._traversing[(tmp_position, tmp_cost_from_start)] = (position, cost_from_start)
-                #
-                #     log(f'{tmp_cost_from_start + measure_distance(tmp_position, self._goal_position)}\t{tmp_position}\t{tmp_cost_from_start}')
-                #     for possible_dir in ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']:
-                #         if self._distances[tmp_position].__dict__[possible_dir] != 0:
-                #             self._heap.push(tmp_cost_from_start + measure_distance(tmp_position, self._goal_position),
-                #                             (tmp_position[0], tmp_position[1], possible_dir),
-                #                             tmp_cost_from_start)
-                #     self._visited.add(tmp_position)
+            for new_dir in self._dirs_to_check[dir]:
+    
+                distance = self._distances[position].__dict__[new_dir]
+                abs_distance = abs(distance)  # Ignore the fact it can be to the wall.
+                xd = yd = 0
+                if 'N' in new_dir:
+                    yd = -1
+                if 'S' in new_dir:
+                    yd = 1
+                if 'E' in new_dir:
+                    xd = 1
+                if 'W' in new_dir:
+                    xd = -1
                 
-            new_position = (position[0] + distance * xd, position[1] + distance * yd)
-            if new_position in self._visited:
-                pass
-
-            new_cost_from_start = cost_from_start + measure_distance(position, new_position)
-            new_heuristic_value = new_cost_from_start + measure_distance(new_position, self._goal_position)
-
-            log(f'{new_cost_from_start + measure_distance(new_position, self._goal_position)}\t{new_position}\t{new_cost_from_start}')
-            for possible_dir in ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']:
-                if self._distances[new_position].__dict__[possible_dir] != 0:
-                    self._heap.push(new_heuristic_value, (new_position[0], new_position[1], possible_dir), new_cost_from_start)
-            self._traversing[(new_position, new_cost_from_start)] = (position, cost_from_start)
-            self._visited.add(new_position)
+                next_position = None
+                next_cost = None
+                
+                goal_xd, goal_yd = self._goal_position[0] - x, self._goal_position[1] - y
+                is_in_general_dir = False
+                if goal_xd == 0:
+                    is_in_exact_dir = (goal_yd > 0 and new_dir == 'S') or (goal_yd < 0 and new_dir == 'N')
+                elif goal_yd == 0:
+                    is_in_exact_dir = (goal_xd > 0 and new_dir == 'E') or (goal_xd < 0 and new_dir == 'W')
+                else:
+                    is_in_exact_dir = ((goal_xd < 0 and goal_yd < 0 and new_dir == 'NW') or
+                                       (goal_xd < 0 and goal_yd > 0 and new_dir == 'SW') or
+                                       (goal_xd > 0 and goal_yd < 0 and new_dir == 'NE') or
+                                       (goal_xd > 0 and goal_yd > 0 and new_dir == 'SE'))
+                    is_in_general_dir = (
+                        (self._goal_position[0] < x and self._goal_position[1] < y and new_dir == 'NW') or
+                        (self._goal_position[0] < x and self._goal_position[1] > y and new_dir == 'SW') or
+                        (self._goal_position[0] > x and self._goal_position[1] < y and new_dir == 'NE') or
+                        (self._goal_position[0] > x and self._goal_position[1] > y and new_dir == 'SE')
+                    )
+                if (
+                    len(new_dir) == 1 and
+                    is_in_exact_dir and
+                    measure_distance(position, self._goal_position) <= abs(self._distances[position].__dict__[new_dir])
+                ):
+                    next_position = self._goal_position
+                    next_cost = cost_from_start + measure_distance(position, self._goal_position)
+                elif (
+                    len(new_dir) == 2 and
+                    is_in_general_dir and
+                    (abs(self._goal_position[0] - x) <= abs_distance or (abs(self._goal_position[1] - y) <= abs_distance))
+                ):
+                    min_diff = min(abs(self._goal_position[0] - x), abs(self._goal_position[1] - y))
+                    next_position = (x + min_diff * xd, y + min_diff * yd)
+                    next_cost = cost_from_start + np.sqrt(2) * min_diff
+                elif self._distances[position].__dict__[dir] > 0:
+                    next_position = (x + distance * xd, y + distance * yd)
+                    if len(dir) == 1:
+                        next_cost = cost_from_start + distance
+                    else:
+                        next_cost = cost_from_start + distance * np.sqrt(2)
+                
+                if next_position is not None:
+                    heuristic_value = next_cost + heuresis_distance(next_position, self._goal_position)
+                    self._heap.push(heuristic_value, (next_position[0], next_position[1], new_dir), next_cost)
+                    log(f'push\t{heuristic_value}, {next_position[0], next_position[1], new_dir}, {next_cost}')
+                    self._traversing[(next_position, next_cost)] = (position, cost_from_start)
     
     
     def find_next_node(self, time_limit: float) -> Optional[Tuple[Tuple[int, int], int]]:
@@ -156,14 +180,20 @@ class JPS:
             value, (x, y, dir), cost = self._heap.pop_push_min()
             position = (x, y)
             log(f'found\tlast: {position} {cost}')
+            log(f'current position: {self._position}')
+            log(f'starting pos: {position}')
+            log(f'traversing: {self._traversing}')
             prev_position, prev_cost = position, cost
             while position != self._position:
                 prev_position, prev_cost = position, cost
                 position, cost = self._traversing[(position, cost)]
             
-            self._position = position
-            self._distance_from_start = cost
             return prev_position, prev_cost
+    
+    def set_state(self, position: Tuple[int, int], distance_from_start: float) -> None:
+        self._position = position
+        log(f'set new pos: {self._position}')
+        self._distance_from_start = distance_from_start
 
 
 width, height = [int(i) for i in input().split()]
@@ -192,13 +222,14 @@ while True:
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
+    current_position = jps._position
     next_node = jps.find_next_node(0.05)
     if next_node is None:
         print('NO PATH')
     else:
         position, distance_from_start = next_node
-        print(f'{position[0]} {position[1]} {jps._position[0]} {jps._position[1]} {distance_from_start}')
-    if jps._found_path:
-        break
+        print(f'{position[0]} {position[1]} {current_position[0]} {current_position[1]} {distance_from_start}')
+        jps.set_state(position, distance_from_start)
+
     # In order of nodes visited by the JPS+ algorithm, a line containing "nodeColumn nodeRow parentColumn parentRow givenCost".
     # print("3 4 0 2 3.14")
